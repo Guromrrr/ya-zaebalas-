fff8038eb943a0ec75a92f1c33f61fead9031390
Masha Markova <markova.ms00@yandex.ru>
2017-08-07 03:32:42 +0700

    Calc1

--- CEV.java
+++ CEV.java
@@ -8,10 +8,65 @@ import java.awt.event.ActionListener;
 
 public class CEV implements ActionListener {
 
+    FC parent;
+
+    char selectedAction = ' '; //- + * /  {
+    double currentResult = 0;
+    CEV (FC parent) {
+        this.parent = parent;
+    }
+
 
     public void actionPerformed(ActionEvent e) {
 
-JOptionPane.showConfirmDialog(null, "Text", "test", JOptionPane.PLAIN_MESSAGE);
+        JButton clickedButton = (JButton) e.getSource();
+        String dispFieldText = parent.displayField.getText();
+        double displyaValue = 0;
+        if (!"".equals(dispFieldText)) {
+            displyaValue = Double.parseDouble(dispFieldText);
+        }
+        Object src = e.getSource();
+        if (src == parent.buttonPlus) {
+            selectedAction = '+';
+            currentResult = displyaValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonMinus){
+        selectedAction = '-';
+        currentResult = displyaValue;
+        parent.displayField.setText("");
+
+
+        } else if (src == parent.buttonDevide){
+            selectedAction = '/';
+            currentResult = displyaValue;
+            parent.displayField.setText("");
+
+        } else if (src == parent.buttonMultply){
+            selectedAction = '*';
+            currentResult = displyaValue;
+            parent.displayField.setText("");
+
+        } else if (src == parent.buttonEqual) {
+            if (selectedAction == '+') {
+                currentResult += displyaValue;
+                parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '-'){
+                currentResult -= displyaValue;
+                parent.displayField.setText("" + currentResult);
+
+            } else if (selectedAction == '/'){
+                currentResult /= displyaValue;
+                parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '*'){
+                currentResult *= displyaValue;
+                parent.displayField.setText("" + currentResult);
+            } else {
+                String clickedButtonLabel = clickedButton.getText();
+                parent.displayField.setText(dispFieldText + clickedButtonLabel);
+            }
+
+        }
+
 
     }
-}
+    }
--- FC.java
+++ FC.java
@@ -6,76 +6,90 @@ import java.awt.*;
 
 public class FC {
 
-    JPanel MFC;
-    JTextField displayField;
-    JButton button0;
-    JButton button1;
-    JButton button2;
-    JButton button3;
-    JButton button4;
-    JButton button5;
-    JButton button6;
-    JButton button7;
-    JButton button8;
-    JButton button9;
-    JButton buttonPoint;
-    JButton buttonEqual;
-    JPanel p1;
+
+    JButton button0 = new JButton("0");
+    JButton button1 = new JButton("1");
+    JButton button2 = new JButton("2");
+    JButton button3 = new JButton("3");
+    JButton button4 = new JButton("4");
+    JButton button5 = new JButton("5");
+    JButton button6 = new JButton("6");
+    JButton button7 = new JButton("7");
+    JButton button8 = new JButton("8");
+    JButton button9 = new JButton("9");
+    JButton buttonPoint = new JButton(".");
+    JButton buttonEqual = new JButton("=");
+
+    JButton buttonPlus = new JButton("+");
+    JButton buttonMinus = new JButton("-");
+    JButton buttonDevide = new JButton("/");
+    JButton buttonMultply = new JButton("*");
+
+    JPanel MFC = new JPanel();
+    JTextField displayField = new JTextField(30);
 
 
     FC(){
-        MFC = new JPanel();
+       BorderLayout b1 = new BorderLayout();
+       MFC.setLayout(b1);
 
-        BorderLayout b1 = new BorderLayout();
-        MFC.setLayout(b1);
+       MFC.add("North", displayField);
 
-        displayField = new JTextField (30);
-        MFC.add("North", displayField);
-
-
-        button0 = new JButton("0");
-        button1 = new JButton("1");
-        button2 = new JButton("2");
-        button3 = new JButton("3");
-        button4 = new JButton("4");
-        button5 = new JButton("5");
-        button6 = new JButton("6");
-        button7 = new JButton("7");
-        button8 = new JButton("8");
-        button9 = new JButton("9");
-        buttonPoint = new JButton(".");
-        buttonEqual = new JButton("=");
-
-        p1 = new JPanel();
-        GridLayout g1 = new GridLayout(4,3);
-        p1.setLayout(g1);
+       JPanel p1 = new JPanel();
+       GridLayout g1 = new GridLayout(4,3);
 
 
-        p1.add(button1);
-        p1.add(button2);
-        p1.add(button3);
-        p1.add(button4);
-        p1.add(button5);
-        p1.add(button6);
-        p1.add(button7);
-        p1.add(button8);
-        p1.add(button9);
-        p1.add(button0);
-        p1.add(buttonPoint);
-        p1.add(buttonEqual);
+       p1.setLayout(g1);
+       p1.add(button1);
+       p1.add(button2);
+       p1.add(button3);
+       p1.add(button4);
+       p1.add(button5);
+       p1.add(button6);
+       p1.add(button7);
+       p1.add(button8);
+       p1.add(button9);
+       p1.add(button0);
+       p1.add(buttonPoint);
+       p1.add(buttonEqual);
 
-        MFC.add("Center", p1);
+       MFC.add("Center", p1);
 
-        JFrame frame = new JFrame("mnfc");
-        frame.setContentPane(MFC);
+       JPanel p2 = new JPanel();
+       GridLayout g2 = new GridLayout(4,1);
+       p2.setLayout(g2);
+       p2.add(buttonPlus);
+       p2.add(buttonMultply);
+       p2.add(buttonMinus);
+       p2.add(buttonDevide);
 
-        frame.pack();
+       MFC.add("East", p2);
 
-        frame.setVisible(true);
+       JFrame frame = new JFrame("MFNC");
+       frame.setContentPane(MFC);
 
-        HIM him = new HIM();
-        button0.addActionListener(him);
+       frame.pack();
+       frame.setVisible(true);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+       CEV cev = new CEV(this);
+
+       button1.addActionListener(cev);
+       button2.addActionListener(cev);
+       button3.addActionListener(cev);
+       button4.addActionListener(cev);
+       button5.addActionListener(cev);
+       button6.addActionListener(cev);
+       button7.addActionListener(cev);
+       button8.addActionListener(cev);
+       button9.addActionListener(cev);
+       button0.addActionListener(cev);
+       buttonPoint.addActionListener(cev);
+       buttonPlus.addActionListener(cev);
+       buttonMultply.addActionListener(cev);
+       buttonMinus.addActionListener(cev);
+       buttonDevide.addActionListener(cev);
+       buttonEqual.addActionListener(cev);
 
     }
 
